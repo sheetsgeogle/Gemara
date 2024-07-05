ba935951d4e645606aefdd2dc65508653da6ab45
sheetsgeogle <vhsv3576@gmail.com>
2024-07-05 17:54:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,9 +51,10 @@ elif option == 'English':
 if full_hebrew_name:
     def download_font(url, filename):
         try:
+            # Download the font file
             response = requests.get(url)
             response.raise_for_status()  # Raise an exception for HTTP errors
-
+            
             # Ensure the directory exists
             os.makedirs(os.path.dirname(filename), exist_ok=True)
             
@@ -70,28 +71,28 @@ if full_hebrew_name:
         pdf_file = "Hebrew_Name.pdf"  # Use a relative path or an appropriate location
         font_path = "SBL_Hbrw (1).ttf"  # Use a relative path or an appropriate location
         try:
-            c = canvas.Canvas(pdf_file, pagesize=letter)
-            width, height = letter
-
-            # Register the SBL Hebrew font
             if os.path.exists(font_path):
+                c = canvas.Canvas(pdf_file, pagesize=letter)
+                width, height = letter
+
+                # Register the SBL Hebrew font
                 pdfmetrics.registerFont(TTFont('SBL_Hebrew', font_path))
                 st.info(f"Font registered from {font_path}.")
+
+                # Draw the black text
+                c.setFont("SBL_Hebrew", 41)
+                c.drawString(100, height - 100, "פרקי המשנית של אותיות השם")
+
+                # Draw the gold text
+                c.setFont("SBL_Hebrew", 86)
+                c.setFillColor(HexColor("#be9a63"))
+                c.drawString(100, height - 200, name)
+
+                c.save()
+                return pdf_file
             else:
                 st.error(f"Font file not found at {font_path}.")
                 return None
-
-            # Draw the black text
-            c.setFont("SBL_Hebrew", 41)
-            c.drawString(100, height - 100, "פרקי המשנית של אותיות השם")
-
-            # Draw the gold text
-            c.setFont("SBL_Hebrew", 86)
-            c.setFillColor(HexColor("#be9a63"))
-            c.drawString(100, height - 200, name)
-
-            c.save()
-            return pdf_file
         except Exception as e:
             st.error(f"An error occurred while creating the PDF: {e}")
             return None
